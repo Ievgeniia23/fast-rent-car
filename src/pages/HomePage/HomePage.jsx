c68b3c3dbfbc2c417b43829bf82276919cbad87c
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2025-04-02 22:47:57 +0200

    Add Home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import css from "./HomePage.module.css"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className={css.homeWrapper}>
+      <h1>Find your perfect rental car</h1>
+      <p>Reliable and budget-friendly rentals for any journey</p>
 
-export default HomePage
+      <Link to="/catalog">
+        <button type="button">View Catalog</button>
+      </Link>
+    </div>
+  );    
+};
+
+export default HomePage;
